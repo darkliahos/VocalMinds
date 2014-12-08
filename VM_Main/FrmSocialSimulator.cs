7b9261ae0e1613d7b8c70ba6f261ff0c59db985b
Sohail Nasir <jtdinc2000@yahoo.co.uk>
2014-12-08 22:39:29 +0000

    #11 Handled Story and sketch events in the social Simulator

--- VM_Main/FrmSocialSimulator.cs
+++ VM_Main/FrmSocialSimulator.cs
@@ -4,23 +4,11 @@ using VM_Model;
 
 namespace VM_Main
 {
-/*
- Some quick notes:
- * Axwindowsmediaplayer is the name of the control
- * the first button submits what the user just said 
- (But hopefully I will be able to optomise it so the user just 
-  speaks and the program automaitcally takes it on board, it will have 
-  to be an on initinailise event)
- * the second button repeats what the simulation just said
- * the third button kicks off the simulator
- * Added voice recognition
- * Added an array that calls parts of the simulation
- */
     public partial class FrmSocialSimulator : Form
     {
         private readonly VideoScenario _videoScenario;
-        private int currentPlayOrder = 1;
-        private string currentUri = "";
+        private int _currentPlayOrder = 1;
+        private string _currentUri = "";
         private VideoSegment _videoSegment;
 
         public FrmSocialSimulator(VideoScenario videoScenario)
@@ -39,9 +27,23 @@ namespace VM_Main
         {
             foreach (var videoSegment in _videoScenario.VideoSegment)
             {
-                if (videoSegment.PlayOrder == currentPlayOrder)
+                if (videoSegment.PlayOrder == ModelConstants.PlayOrderStory)
                 {
-                    currentUri = videoSegment.VideoPath;
+                    var story = new FrmStory();
+                    story.Show();
+                    break;
+                }
+
+                if (videoSegment.PlayOrder == ModelConstants.PlayOrderStory)
+                {
+                    var scribblePad = new FrmScribblePad();
+                    scribblePad.Show();
+                    break;
+                }
+
+                if (videoSegment.PlayOrder == _currentPlayOrder)
+                {
+                    _currentUri = videoSegment.VideoPath;
                     _videoSegment = videoSegment;
                 }
             }
@@ -49,7 +51,7 @@ namespace VM_Main
 
         private void btnPlay_Click(object sender, System.EventArgs e)
         {
-            visualSocialInterface.URL = currentUri;
+            visualSocialInterface.URL = _currentUri;
             visualSocialInterface.Ctlcontrols.play();
         }
 
@@ -64,9 +66,11 @@ namespace VM_Main
             {
                 if (response == res.Answer)
                 {
-                    currentPlayOrder = res.SocialSimulatorAction;
+                    _currentPlayOrder = res.SocialSimulatorAction;
+                    MessageBox.Show("Well Done");//TODO: Better way of letting the user know if they did well?
+                    SetUpVideoSegment();
+                    break;
                 }
-                //TODO: Handle Story and Drawing responses
             }
         }
 
---
+++ VM_Model/ModelConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VM_Model
+{
+    /// <summary>
+    /// All Model Constants live here
+    /// </summary>
+    public static class ModelConstants
+    {
+        public const int PlayOrderStory = 9999;
+        public const int PlayOrderSketch = 9998;
+    }
+}
