8423792435dc9363a4fe4f23904e28138f8449e8
Sohail Nasir <jtdinc2000@yahoo.co.uk>
2015-03-01 14:27:57 +0000

    #30 Added Missing Files to fix build

---
+++ VMUtils/Exceptions/FileFailedToUnlockedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VMUtils.Exceptions
+{
+    public class FileFailedToUnlockedException : Exception
+    {
+        public FileFailedToUnlockedException()
+        {
+        }
+
+        public FileFailedToUnlockedException(string message)
+            : base(message)
+        {
+        }
+
+        public FileFailedToUnlockedException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ VMUtils/VoiceRecognition/VoiceRecognitionFileProcessor.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using VMUtils.Interfaces;
+using VM_Model;
+
+namespace VMUtils.VoiceRecognition
+{
+    public class VoiceRecognitionFileProcessor : IFileProcessor<VoiceRecognitionScenario, ImportedVoiceRecognitionScenario>
+    {
+        private ImportedVoiceRecognitionScenario _importedScenarios;
+        private readonly IImporter<ImportedVoiceRecognitionScenario> _importer;
+        private readonly string _path;
+
+        public VoiceRecognitionFileProcessor(IImporter<ImportedVoiceRecognitionScenario> importer, string path)
+        {
+            _importer = importer;
+            _path = path;
+            _importedScenarios = importer.LoadFile(path);
+        }
+        public List<VoiceRecognitionScenario> LoadScenarioFromFile()
+        {
+            return _importedScenarios.VoiceRecognitionScenarios;
+        }
+
+        public ImportedVoiceRecognitionScenario LoadScenarioObject()
+        {
+            return _importedScenarios;
+        }
+
+        public ImportedVoiceRecognitionScenario RefreshScenarioObject()
+        {
+            _importedScenarios = _importer.LoadFile(_path);
+            return _importedScenarios;
+        }
+    }
+}
