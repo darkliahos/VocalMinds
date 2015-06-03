5f1a3c6483b551cf6ffca314b60b5e196f9f8621
Sohail Nasir <jtdinc2000@yahoo.co.uk>
2015-06-03 18:41:05 +0100

    #29 Cancellation of Web Version

--- VM.Main.Api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VM.Main.Api.WebApiApplication" Language="C#" %>
---
+++ VM_ScenarioEditor/Voice Recognition/VoiceRecognitionEditor.Designer.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace VM_ScenarioEditor
+{
+    partial class VoiceRecognitionEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAddAnswers = new System.Windows.Forms.Button();
+            this.txtAnswerSingle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtAudioName = new System.Windows.Forms.TextBox();
+            this.lstAnswers = new System.Windows.Forms.ListBox();
+            this.txtCopyrightNotice = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txttitle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblanswers = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblQTitle = new System.Windows.Forms.Label();
+            this.btnSelectContentWizard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(402, 205);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 31;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAddAnswers
+            // 
+            this.btnAddAnswers.Location = new System.Drawing.Point(402, 234);
+            this.btnAddAnswers.Name = "btnAddAnswers";
+            this.btnAddAnswers.Size = new System.Drawing.Size(75, 23);
+            this.btnAddAnswers.TabIndex = 30;
+            this.btnAddAnswers.Text = "Add";
+            this.btnAddAnswers.UseVisualStyleBackColor = true;
+            this.btnAddAnswers.Click += new System.EventHandler(this.btnAddAnswers_Click);
+            // 
+            // txtAnswerSingle
+            // 
+            this.txtAnswerSingle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnswerSingle.Location = new System.Drawing.Point(200, 234);
+            this.txtAnswerSingle.Name = "txtAnswerSingle";
+            this.txtAnswerSingle.Size = new System.Drawing.Size(172, 29);
+            this.txtAnswerSingle.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 28;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(318, 388);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 27;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtAudioName
+            // 
+            this.txtAudioName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAudioName.Location = new System.Drawing.Point(200, 57);
+            this.txtAudioName.Name = "txtAudioName";
+            this.txtAudioName.ReadOnly = true;
+            this.txtAudioName.Size = new System.Drawing.Size(196, 29);
+            this.txtAudioName.TabIndex = 26;
+            // 
+            // lstAnswers
+            // 
+            this.lstAnswers.FormattingEnabled = true;
+            this.lstAnswers.Location = new System.Drawing.Point(200, 107);
+            this.lstAnswers.Name = "lstAnswers";
+            this.lstAnswers.Size = new System.Drawing.Size(172, 121);
+            this.lstAnswers.TabIndex = 25;
+            // 
+            // txtCopyrightNotice
+            // 
+            this.txtCopyrightNotice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCopyrightNotice.Location = new System.Drawing.Point(200, 335);
+            this.txtCopyrightNotice.Name = "txtCopyrightNotice";
+            this.txtCopyrightNotice.Size = new System.Drawing.Size(193, 29);
+            this.txtCopyrightNotice.TabIndex = 24;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAuthor.Location = new System.Drawing.Point(200, 300);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(193, 29);
+            this.txtAuthor.TabIndex = 23;
+            // 
+            // txttitle
+            // 
+            this.txttitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttitle.Location = new System.Drawing.Point(200, 22);
+            this.txttitle.Name = "txttitle";
+            this.txttitle.Size = new System.Drawing.Size(196, 29);
+            this.txttitle.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 335);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 25);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Copyright Notice:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 25);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Author:";
+            // 
+            // lblanswers
+            // 
+            this.lblanswers.AutoSize = true;
+            this.lblanswers.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblanswers.Location = new System.Drawing.Point(17, 107);
+            this.lblanswers.Name = "lblanswers";
+            this.lblanswers.Size = new System.Drawing.Size(100, 25);
+            this.lblanswers.TabIndex = 19;
+            this.lblanswers.Text = "Answers:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 25);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Audio Path:";
+            // 
+            // lblQTitle
+            // 
+            this.lblQTitle.AutoSize = true;
+            this.lblQTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQTitle.Location = new System.Drawing.Point(17, 19);
+            this.lblQTitle.Name = "lblQTitle";
+            this.lblQTitle.Size = new System.Drawing.Size(59, 25);
+            this.lblQTitle.TabIndex = 17;
+            this.lblQTitle.Text = "Title:";
+            // 
+            // btnSelectContentWizard
+            // 
+            this.btnSelectContentWizard.Location = new System.Drawing.Point(402, 61);
+            this.btnSelectContentWizard.Name = "btnSelectContentWizard";
+            this.btnSelectContentWizard.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectContentWizard.TabIndex = 32;
+            this.btnSelectContentWizard.Text = "Select";
+            this.btnSelectContentWizard.UseVisualStyleBackColor = true;
+            this.btnSelectContentWizard.Click += new System.EventHandler(this.btnSelectContentWizard_Click);
+            // 
+            // VoiceRecognitionEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 431);
+            this.Controls.Add(this.btnSelectContentWizard);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAddAnswers);
+            this.Controls.Add(this.txtAnswerSingle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtAudioName);
+            this.Controls.Add(this.lstAnswers);
+            this.Controls.Add(this.txtCopyrightNotice);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txttitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblanswers);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblQTitle);
+            this.Name = "SocialSimulatorEditor";
+            this.Text = "VoiceRecognitionEditor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAddAnswers;
+        private System.Windows.Forms.TextBox txtAnswerSingle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtAudioName;
+        private System.Windows.Forms.ListBox lstAnswers;
+        private System.Windows.Forms.TextBox txtCopyrightNotice;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txttitle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblanswers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblQTitle;
+        private System.Windows.Forms.Button btnSelectContentWizard;
+    }
+}
